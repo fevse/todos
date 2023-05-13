5e5221ec785a851284773d6db182959da415352d
fevse <fedulov.v.s@gmail.com>
2023-05-13 18:58:53 +0600

    add TodoItem

--- model.py
+++ model.py
@@ -12,3 +12,14 @@ class Todo(BaseModel):
                 "item": "for example"
             }
         }
+
+
+class TodoItem(BaseModel):
+    item: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
